20f5a146319d6e8424e6f0f2e897fae3620b7c7c
jjavier <razornice@gmail.com>
2023-01-04 10:29:58 -0300

    initial bishop branch commit

---
+++ lib/bishop.rb
@@ -0,0 +1,11 @@
+class Bishop
+  attr_accessor : position
+  def initialize(position)
+    # position in an array on two interger y and x (0-7)
+    @position = position
+    
+  end
+  def move_to(y,x)
+    ``
+  end
+end
