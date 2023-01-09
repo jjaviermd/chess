14663fbe0dca374f2c0ddc949edffebf25136ebe
jjavier <razornice@gmail.com>
2023-01-09 18:37:51 -0300

    early move_to method on  queen branch

--- lib/queen.rb
+++ lib/queen.rb
@@ -7,5 +7,20 @@ defclass Queen\
   
   def move_to(y,x)
     
+    if y != self.position.first && x != self.position.last # non lineaer move for a rook
+      #raise error, a rook can't move in that direction
+      puts "error"
+    elsif y == self.position[0]
+      @position[-1] = x
+    else
+      # x == self.position.last
+      @position[0] = y
+    end
+
+  else
+    puts "out of Boundaries"
   end
+  
+
+
 end
