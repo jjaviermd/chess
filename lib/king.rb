ee42e04316c11ff5f83ff158ce1a22f851483230
jjavier <razornice@gmail.com>
2023-01-10 09:37:14 -0300

    class king can_go? method

--- lib/king.rb
+++ lib/king.rb
@@ -1,13 +1,18 @@
+require "pry-byebug"
+
 class King
 
   attr_accessor :position
+  attr_reader :moves
 
   def initialize(position)
     @position = position
-    @moves = [[1,0],[1,-1],[1,1],[-1,0],[-1,-1],[1,1],[0,1],[0,-1]].freeze
+    @moves = [[1,0],[1,-1],[1,1],[-1,0],[-1,-1],[1,1],[0,1],[0,-1],[-1,1]].freeze
   end
 
   def can_go?(y,x)
-
+    dif = [self.position.first - y, self.position.last - x]
+    return true if self.moves.include?(dif)
+    return false
   end
 end
