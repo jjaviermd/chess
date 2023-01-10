f944b7851bb9c0e773ab8a3b01b912ee7ce2469d
jjavier <razornice@gmail.com>
2023-01-09 21:48:12 -0300

    can_go? mothod

--- lib/queen.rb
+++ lib/queen.rb
@@ -1,4 +1,4 @@
-defclass Queen\
+class Queen
   attr_accessor :position
 
   def initialize(position)
@@ -6,14 +6,17 @@ defclass Queen\
   end
   
   def can_go?(y,x)
-    # rook-like move
-    if y != self.position.first && x != self.position.last 
-      return false
+    y_array = [self.position.first, y]
+    x_array = [self.position.last, x]
+
+    return true if y_array.max - y_array.min == x_array.max - x_array.min
+
+    if y == self.position.first && x == self.position.last 
+      return true
     elsif y == self.position[0] ||x == self.position.last
       return true
-    end 
+    else return false
+    end
   end 
   
-
-
 end
