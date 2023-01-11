548e14fd26cf76636be7e386277f76c10b0c3617
jjavier <razornice@gmail.com>
2023-01-11 09:50:14 -0300

    white pawn and black pawn classes and can_go? method

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -1,11 +1,30 @@
+require "pry-byebug"
 class Pawn
 
-  attr_accessor :position, :first_move?
+  attr_accessor :position
 
   def initialize(position)
     @position = position
-    @first_move? = false 
   end
 
-  
 end
+
+class WhitePawn < Pawn
+  def can_go?(y,x)
+    return true if self.position.first - y == 1 && x == self.position.last
+    return true if self.position.first - y == 2 && x == self.position.last && self.position.first == 6
+    return false
+  end
+end
+
+class BlackPawn < Pawn
+  def can_go?(y,x)
+    return true if y - self.position.first == 1 && x == self.position.last
+    return true if y - self.position.first == 2 && x == self.position.last && self.position.first == 1
+    return false
+  end
+end
+
+b_pawn = BlackPawn.new([1,2])
+binding pry
+p un_pawn.position
