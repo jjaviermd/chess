236f5b1ec1867eaeae101103f59771f8b400a456
jjavier <razornice@gmail.com>
2023-02-01 11:53:56 -0300

    reform pawn

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -11,6 +11,7 @@ end
 class WhitePawn < Pawn
   def can_go?(y,x)
     return true if self.position.first - y == 1 && x == self.position.last
+    return true if self.position.first - y == 1 && (self.position.last - x).abs == 1
     return true if self.position.first - y == 2 && x == self.position.last && self.position.first == 6
     return false
   end
@@ -19,6 +20,7 @@ end
 class BlackPawn < Pawn
   def can_go?(y,x)
     return true if y - self.position.first == 1 && x == self.position.last
+    return true if y - self.position.first == 1 && (self.position.last - x).abs == 1
     return true if y - self.position.first == 2 && x == self.position.last && self.position.first == 1
     return false
   end
